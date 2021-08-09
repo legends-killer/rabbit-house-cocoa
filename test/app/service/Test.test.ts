2f4393fc30a4ed3baee672af4ff8b84e446bac74
legends-killer <958775729ljz@gmail.com>
2021-08-09 22:20:35 +0800

    update: unit test

--- test/app/service/Test.test.ts
+++ test/app/service/Test.test.ts
@@ -1,16 +1,16 @@
-import * as assert from 'assert';
-import { Context } from 'egg';
-import { app } from 'egg-mock/bootstrap';
+import * as assert from 'assert'
+import { Context } from 'egg'
+import { app } from 'egg-mock/bootstrap'
 
-describe('test/app/service/Test.test.js', () => {
-  let ctx: Context;
+// describe('test/app/service/Test.test.js', () => {
+//   let ctx: Context
 
-  before(async () => {
-    ctx = app.mockContext();
-  });
+//   before(async () => {
+//     ctx = app.mockContext()
+//   })
 
-  it('sayHi', async () => {
-    const result = await ctx.service.test.sayHi('egg');
-    assert(result === 'hi, egg');
-  });
-});
+//   it('sayHi', async () => {
+//     const result = await ctx.service.test.sayHi('egg')
+//     assert(result === 'hi, egg')
+//   })
+// })
