8e667095b25c371aed51a563f44f16422a284983
legends-killer <958775729ljz@gmail.com>
2021-08-12 23:22:58 +0800

    clean up

--- config/config.default.ts
+++ config/config.default.ts
@@ -47,14 +47,8 @@ export default (appInfo: EggAppInfo) => {
   // add your egg config in here
   config.middleware = []
 
-  // add your special config in here
-  const bizConfig = {
-    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  }
-
   // the return config will combines to EggAppConfig
   return {
     ...config,
-    ...bizConfig,
   }
 }
--- config/plugin.ts
+++ config/plugin.ts
@@ -1,16 +1,6 @@
 import { EggPlugin } from 'egg'
 
 const plugin: EggPlugin = {
-  // static: true,
-  // nunjucks: {
-  //   enable: true,
-  //   package: 'egg-view-nunjucks',
-  // },
-  // emqtt: { enable: true, package: 'egg-emqtt' },
-  // cors: {
-  //   enable: true,
-  //   package: 'egg-cors',
-  // },
   rabbitHouseMqttPlugin: { enable: true, package: 'egg-rabbit-house-mqtt-plugin' },
   typeorm: {
     enable: true,
