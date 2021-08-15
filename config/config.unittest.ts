99ade57284d844044afaba879cf9b70f1bf8b533
legends-killer <958775729ljz@gmail.com>
2021-08-15 23:55:21 +0800

    fix: ci config

--- config/config.unittest.ts
+++ config/config.unittest.ts
@@ -13,5 +13,30 @@ export default () => {
       prepareDatabase: () => {},
     },
   }
+  config.rabbitHouseMqttPlugin = {
+    server: {
+      host: '127.0.0.1',
+      port: 1883,
+      username: 'user',
+      password: '123456',
+    },
+    client: {
+      host: '127.0.0.1',
+      clientId: 'egg',
+      username: 'user',
+      password: '123456',
+      protocol: 'mqtt',
+      msgMiddleware: ['msg2json'],
+      options: {
+        keepalive: 60,
+        protocolId: 'MQTT',
+        protocolVersion: 4,
+        clean: true,
+        reconnectPeriod: 1000,
+        connectTimeout: 30 * 1000,
+        rejectUnauthorized: false,
+      },
+    },
+  }
   return config
 }
