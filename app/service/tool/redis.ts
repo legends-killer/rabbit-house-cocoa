976761f3bb19823927b9237c365ca6090c4fb09e
legends-killer <958775729ljz@gmail.com>
2021-08-24 19:43:45 +0800

    update: redis methods

--- app/service/tool/redis.ts
+++ app/service/tool/redis.ts
@@ -1,37 +1,61 @@
-import { Service, Context, Singleton } from 'egg'
-import IORedis = require('ioredis')
+import { Service, Singleton } from 'egg'
+import { Redis } from 'ioredis'
 
-export default class Redis extends Service {
-  private redis: IORedis.Redis & Singleton<IORedis.Redis>
-  constructor(ctx: Context) {
-    super(ctx)
-    this.redis = ctx.app.redis
-  }
-  async set(key: string, value: any, seconds = 86400) {
-    // append timestrap to redis array
+export default class RedisTool extends Service {
+  private redis = this.app.redis as Singleton<Redis> & Redis
+  async set(db: string, key: string, value: any, seconds?: number) {
+    const { redis } = this
     value.timeOnRedis = new Date()
     value = JSON.stringify(value)
     if (!seconds) {
-      await this.redis.rpush(key, value)
+      await redis.get(db).set(key, value)
     } else {
-      await this.redis.expire(key, seconds)
-      await this.redis.rpush(key, value)
+      await redis.get(db).expire(key, seconds)
+      return await redis.get(db).set(key, value)
     }
   }
-  async get(key: string) {
-    const { redis } = this.app
-    const data = await redis.lrange(key, 0, -1)
+
+  async get(db: string, key: string) {
+    const { redis } = this
+    const data = await redis.get(db).get(key)
+    if (!data) return
+    return JSON.parse(data)
+  }
+
+  async delete(db: string, key: string) {
+    return await this.redis.get(db).del(key)
+  }
+
+  async setArr(db: string, key: string, value: any, seconds?: number) {
+    // append timestrap to redis array
+    const { redis } = this
+    value.timeOnRedis = new Date()
+    value = JSON.stringify(value)
+    if (!seconds) {
+      await redis.get(db).rpush(key, value)
+    } else {
+      await redis.get(db).expire(key, seconds)
+      return await redis.get(db).rpush(key, value)
+    }
+  }
+
+  async getArr(db: string, key: string) {
+    const { redis } = this
+    const data = await redis.get(db).lrange(key, 0, -1)
     if (!data) return
     return data.map((d) => {
       return JSON.parse(d)
     })
   }
-  async getAllKey() {
-    return await this.redis.keys('*')
+
+  async getAllKey(db: string) {
+    const { redis } = this
+    return await redis.get(db).keys('*')
   }
-  async flushAll() {
-    const { redis } = this.app
-    redis.flushall()
+
+  async flushAll(db: string) {
+    const { redis } = this
+    redis.get(db).flushall()
     return
   }
 }
