29037c80d99e54eb127697e1209b12056b5a49a3
legends-killer <958775729ljz@gmail.com>
2021-09-10 17:39:46 +0800

    fix: job assembler infinite recursion

--- app/service/mqtt/job.ts
+++ app/service/mqtt/job.ts
@@ -7,13 +7,14 @@ export default class JobService extends Service {
    * @param {number} jobId jobId
    * @return {Array<IScheduleWork>} schedule work array
    */
-  async assembleJob(jobId: number) {
+  async assembleJob(jobId: number, ids: number[] = []) {
+    ids.push(jobId)
     const jobs = await this.service.job.index({ id: jobId })
     if (jobs[1] === 0) this.ctx.logger.error(`Job Not Found, id: ${jobId}`)
     const result = [] as Array<IJobWork>
     for (const work of jobs[0][0].work) {
       if (typeof work === 'number') {
-        result.push(...(await this.assembleJob(work)))
+        if (!ids.includes(work)) result.push(...(await this.assembleJob(work, ids)))
       } else {
         result.push(work)
       }
